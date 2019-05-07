f85bcd6b2852c0d0b002b5ff18b5b9b081b0f50b
Tyler Menezes <tylermenezes@gmail.com>
2019-05-06 22:18:47 -0400

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Metadata from "../components/metadata"
+
+import ox from "./404.jpg"
 
 class NotFoundPage extends React.Component {
   render() {
     return (
       <Layout>
         <Metadata title="Not found!" />
+        <img src={ ox } alt="A confused ox" style={{maxWidth: "400px", marginTop: "2rem"}} />
         <h1>Not found!</h1>
         <p>The page you're looking for doesn&#39;t exist... such sadness.</p>
       </Layout>
